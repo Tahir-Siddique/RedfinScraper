934e9c82143ce9d7fdaf013ca9e8e456c99bd595
Tahir Siddique <82741746+Tahir-Siddique@users.noreply.github.com>
2023-09-11 18:43:04 +0000

    Updated

--- utils/utils.py
+++ utils/utils.py
@@ -3,69 +3,71 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
+import random
 import smtplib
 import time
 from bs4 import BeautifulSoup
 
 import requests
 
+from utils.constants import PROXIES
+
+
+url = "https://www.redfin.com"
+
 
 def get_owner_by_lake_county(item):
     headers = {
-            'user-agent': 'Redfin Android 458.0.1',
+        'user-agent': 'Redfin Android 458.0.1',
     }
-    # print()
-    # print(item.get("url"))
+    item["owner_name"] = "Not found"
+    item["parcel_no"] = "Not found"
     
-    response = requests.get("https://www.redfin.com"+item.get("url"), headers=headers)
+    for proxy in random.sample(PROXIES, len(PROXIES)):
+        proxy_url = f"{proxy['type']}://{proxy['ip']}:{proxy['port']}"
+        try:
+            response = requests.get(url + item.get("url"), headers=headers, proxies={"http": proxy_url, "https": proxy_url}, timeout=5)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, features="html.parser")
+                elems = soup.find_all("div", {"class": "amenity-group"})
+                parcelNumber = "Parcel Number"
+                for elem in elems:
+                    elem.getText(strip=True)
+                    for li in elem.find("ul").find_all("li"):
+                        if parcelNumber in li.getText(strip=True):
+                            item["parcel_no"] = li.getText(strip=True).split(":")[1].strip()
+                
+                # if "Parcel Number" not in parcelNumber:
+                #     try:
+                #         response = requests.get('https://kaneil.devnetwedge.com/parcel/view/'+parcelNumber, headers=headers, allow_redirects=True, proxies={"http": proxy_url, "https": proxy_url}, timeout=5)
+                #         soup = BeautifulSoup(response.text, "html.parser")
+                #         owner_name = soup.find("table").find("tr").find_all("td")[2].find_all("div")[1].getText().strip()
+                #         item["owner_name"] = owner_name
+                #         item["parcel_no"] = parcelNumber
+                #         item.pop("get_owner")
+                #         return item
+                #     except Exception as e:
+                #         print(f"Error while getting owner details with proxy {proxy_url}: {str(e)}")
+                # else:
+                #     print(f"Parcel Number not found with proxy {proxy_url}")
+                return item
+            else:
+                print(f"Request failed with proxy {proxy_url} - Status Code: {response.status_code}")
+        except Exception as e:
+            print(f"Error while making request with proxy {proxy_url}: {str(e)}")
     
-    # async with session.get(url="https://www.redfin.com"+item.get("url"), headers=headers) as res:
-        
-        # html = await res.text()
-        # soup = BeautifulSoup(html, features="html.parser")
-    soup = BeautifulSoup(response.text, features="html.parser")
-    elems = soup.find_all("div", {"class":"amenity-group"})
-    parcelNumber = "Parcel Number"
-    for elem in elems:
-        elem.getText(strip=True)
-        for li in elem.find("ul").find_all("li"):
-            if parcelNumber in li.getText(strip=True):
-
-                parcelNumber = li.getText(strip=True).split(":")[1].strip()
-                item["parcel_no"] = parcelNumber
-    # if "Parcel Number" not in parcelNumber:
-
-    #     try:
-    #         response = requests.get('https://kaneil.devnetwedge.com/parcel/view/'+parcelNumber, headers=headers, allow_redirects=True)
-    #         soup = BeautifulSoup(response.text, "html.parser")
-    #         owner_name = soup.find("table").find("tr").find_all("td")[2].find_all("div")[1].getText().strip()
-    #         # print(owner_name)
-    #         item["owner_name"] = owner_name
-    #         item["parcel_no"] = parcelNumber
-    #         item.pop("get_owner")
-    #         return item
-    #     except:
-    #         item["owner_name"] = "Not found"
-    if "owner_name" not in item:
-        item["owner_name"] = "Not found"
-    if "parcel_no" not in item:
-        item["parcel_no"] = "Not found"
     return item
+
 
 
 def get_owner_by_kane_county(item):
     headers = {
             'user-agent': 'Redfin Android 458.0.1',
     }
-    # print()
-    # print(item.get("url"))
     
+    item["owner_name"] = "Not found"
+    item["parcel_no"] = "Not found"
     response = requests.get("https://www.redfin.com"+item.get("url"), headers=headers)
-    
-    # async with session.get(url="https://www.redfin.com"+item.get("url"), headers=headers) as res:
-        
-        # html = await res.text()
-        # soup = BeautifulSoup(html, features="html.parser")
     soup = BeautifulSoup(response.text, features="html.parser")
     elems = soup.find_all("div", {"class":"amenity-group"})
     parcelNumber = "Parcel Number"
@@ -76,7 +78,6 @@ def get_owner_by_kane_county(item):
 
                 parcelNumber = li.getText(strip=True).split(":")[1].strip()
     if "Parcel Number" not in parcelNumber:
-
         try:
             response = requests.get('https://kaneil.devnetwedge.com/parcel/view/'+parcelNumber, headers=headers, allow_redirects=True)
             soup = BeautifulSoup(response.text, "html.parser")
@@ -87,11 +88,7 @@ def get_owner_by_kane_county(item):
             item.pop("get_owner")
             return item
         except:
-            item["owner_name"] = "Not found"
-    if "owner_name" not in item:
-        item["owner_name"] = "Not found"
-    if "parcel_no" not in item:
-        item["parcel_no"] = "Not found"
+            pass
     return item
 
 def send_mail(file_name):
